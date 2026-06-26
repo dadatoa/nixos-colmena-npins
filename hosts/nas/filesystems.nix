26bbd0a3b2c2218d4798cb7919f4c250fadc10d0
dadatoa <f0a202@outlook.com>
2026-06-26 21:04:04 +0700

    [nas] change disk by uuid to xvda (vm)

--- hosts/nas/filesystems.nix
+++ hosts/nas/filesystems.nix
@@ -2,30 +2,46 @@
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
-    options = [ "size=25%" "mode=755" ]; # mode=755 so only root can write to those files
-    };
+    options = [
+      "size=25%"
+      "mode=755"
+    ]; # mode=755 so only root can write to those files
+  };
 
   fileSystems."/nix" = {
     neededForBoot = true;
-    device = "/dev/disk/by-uuid/4176eb8b-ab69-4313-bed8-dea05679a316";
+    device = "/dev/xvda";
     fsType = "btrfs";
-    options = ["subvol=nix" "compress=zstd" "noatime"];
+    options = [
+      "subvol=nix"
+      "compress=zstd"
+      "noatime"
+    ];
   };
   fileSystems."/persist" = {
     neededForBoot = true;
-    device = "/dev/disk/by-uuid/4176eb8b-ab69-4313-bed8-dea05679a316";
+    device = "/dev/xvda";
     fsType = "btrfs";
-    options = ["subvol=persist" "compress=zstd" "noatime"];
+    options = [
+      "subvol=persist"
+      "compress=zstd"
+      "noatime"
+    ];
   };
   fileSystems."/boot" = {
     neededForBoot = true;
-    device = "/dev/disk/by-uuid/4176eb8b-ab69-4313-bed8-dea05679a316";
+    device = "/dev/xvda";
     fsType = "btrfs";
-    options = ["subvol=boot" "noatime"];
+    options = [
+      "subvol=boot"
+      "noatime"
+    ];
   };
 
-  swapDevices = [{
-    device = "/swap/swapfile";
-    size = 4*1024; # Creates an 4GB swap file
-  }];
+  swapDevices = [
+    {
+      device = "/swap/swapfile";
+      size = 4 * 1024; # Creates an 4GB swap file
+    }
+  ];
 }
