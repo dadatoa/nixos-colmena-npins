a96ebeddad597a4123673b134a5ce5adde0f049e
dadatoa <f0a202@outlook.com>
2026-07-04 10:50:46 +0700

    nushell for user operateur

--- common/users.nix
+++ common/users.nix
@@ -2,19 +2,23 @@
 # - uid 1000
 # - passwordless sudo
 # - may reboot/poweroff without sudo (polkit)
-{ ... }:
+{ pkgs, ... }:
 {
   users.users.operateur = {
     isNormalUser = true;
     uid = 1000;
     description = "Operator";
     extraGroups = [ "wheel" "video" ];
+    shell = pkgs.nushell;
+    packages = with pkgs; [ nushell ];
     openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBA52LLKZPhszwrzrqOwLJ2a2spNzjAn/ls6krE9SM/i dadatoa@dadabook"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHnWrIExo7hWe04wTUUEn6smnx/LRfNtPtatR+NgQlfz SpaceK@dadabook"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF36sv0vHnOUCx8uMWCkwLwpQoBgWP0NzYRhd6+6vr8t deploy_app_to_server_github_actions"
     ];
   };
+
+  environment.shells = [ pkgs.nushell ];
 
   users.users.root.hashedPasswordFile = "/persist/secrets/root-password.txt";
 
