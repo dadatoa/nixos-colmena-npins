7e0ace862ffbf71e554dfc2e6208205f327ca027
dadatoa <f0a202@outlook.com>
2026-07-12 11:10:00 +0700

    fit remote access confs with new orbstack nix vm

---
+++ colmena/common/remote.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.mosh.enable = true;
+  # start ssh-agent
+  programs.ssh.startAgent = true;
+
+  services.openssh.enable = true;
+
+  # Enable mDNS autodiscovery
+  services.avahi = {
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+    enable = true;
+    openFirewall = true;
+    nssmdns4 = true;
+  };
+
+}
