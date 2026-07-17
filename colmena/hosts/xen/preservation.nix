6bf4f1aa18f2758bb0fe03cc1685496477480bb1
dadatoa <f0a202@outlook.com>
2026-07-17 15:00:28 +0700

    match documetation with configs

--- colmena/hosts/xen/preservation.nix
+++ colmena/hosts/xen/preservation.nix
@@ -18,8 +18,8 @@
       files = [
         "/etc/ssh/authorized_keys.d/operateur"
         "/etc/xen/auto/bender.cfg"
-        "/etc/xen/auto/nas.cfg"
-        "/etc/xen/auto/deckard.cfg"
+-       "/etc/xen/auto/nas.cfg"
+-       "/etc/xen/auto/deckard.cfg"
         {
           file = "/etc/machine-id";
           inInitrd = true;
