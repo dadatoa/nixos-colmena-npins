c2d083a0257230728a20dfb26e587f523239c59c
dadatoa <f0a202@outlook.com>
2026-08-09 21:17:52 +0700

    disable grow partition

--- flake/vm-config/configuration.nix
+++ flake/vm-config/configuration.nix
@@ -5,7 +5,7 @@
   ];
   # Common configuration for Xen DomU NixOS virtual machines.
   boot = {
-    growPartition = true;
+    growPartition = false; ## c'est là
     kernelParams = [
       "console=hvc0"
       "vga=0x317"
