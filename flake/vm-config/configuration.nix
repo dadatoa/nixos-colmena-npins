60fdd6c83adebc25d8116ed876eed6216dc26f34
dadatoa <f0a202@outlook.com>
2026-08-09 18:58:25 +0700

    change boot options for vms

--- flake/vm-config/configuration.nix
+++ flake/vm-config/configuration.nix
@@ -8,11 +8,12 @@
     growPartition = false;
     kernelParams = [
       "console=hvc0"
-      "vga=0x317"
       "nomodeset"
     ];
     loader.grub.enable = true;
     initrd.systemd.enable = true;
+    # GRUB must not wait forever at the menu in a headless domU.
+    loader.timeout = 5;
   };
   # Grub loader to allow pvh grub usage
   boot.loader.grub.device = "nodev";
