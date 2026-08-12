d74ec837cb86fef0f9c0e43aeca032d8d692a6ba
dadatoa <f0a202@outlook.com>
2026-08-12 19:44:15 +0700

    ssh gpg agent forwarding thru ssh

--- colmena/common/remote.nix
+++ colmena/common/remote.nix
@@ -5,6 +5,9 @@
   programs.ssh.startAgent = true;
 
   services.openssh.enable = true;
+  services.openssh.extraConfig = ''
+    StreamLocalBindUnlink yes
+  '';
 
   services.tailscale = {
     enable = true;
