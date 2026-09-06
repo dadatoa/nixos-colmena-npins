70bdf49b771f9c4ed3c07bd24cb569f50aed9c6c
dadatoa <f0a202@outlook.com>
2026-09-06 23:29:30 +0700

    [nas] podman oci-container qbitorrent fail

--- colmena/hosts/nas-configuration.nix
+++ colmena/hosts/nas-configuration.nix
@@ -89,6 +89,7 @@
         environment = {
           TZ = "Asia/Bangkok";
         };
+        dependsOn = [ "gluetun" ];
         extraOptions = [ "--network container:gluetun" ];
         volumes = [ "qbittorrent.etc:/qbittorrent/etc" "qbittorrent.var:/qbittorrent/var" ];
         image = "quay.io/11notes/qbittorrent:5.2.1";
