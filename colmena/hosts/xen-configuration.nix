7f640906e8d6fd7398fe5d55a0b607e78ab89b91
dadatoa <f0a202@outlook.com>
2026-09-20 10:03:23 +0700

    cockpit allow unencrypted connection

--- colmena/hosts/xen-configuration.nix
+++ colmena/hosts/xen-configuration.nix
@@ -224,7 +224,7 @@
     # openFirewall = true; # Please see the comments section
     settings = {
       WebService = {
-        # AllowUnencrypted = true; # 2026-08-04: Not needed anymore?
+        AllowUnencrypted = true; # 2026-08-04: Not needed anymore?
         Origins = lib.mkForce "http://127.0.0.1:9090 https://127.0.0.1:9090 https://100.113.83.131 https://xen-cockpit.blue-edmontosaurus.ts.net https://100.85.206.102:9090 http://100.85.206.102:9090";
       };
     };
